caf02e473a5615764e4f2699d4cae4baf3d78db3
quangnv <quangnv@qsoft.vn>
2024-05-16 17:44:58 +0700

    test thử formatter

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+
+
+
+
+
+
+
+
+
+
+
+
+        
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
