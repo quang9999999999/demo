e3eb59fba7929526e02126e1934ee16cbfebb4e4
quangnv <quangnv@qsoft.vn>
2024-05-17 11:05:54 +0700

    test pre-commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
+
+
+
+
+
+        
+        // This is the theme of your application
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
         // the application has a blue toolbar. Then, without quitting the app,
